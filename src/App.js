44982800c52aa7582be00a1b6a38da74a51ca725
qatta93 <panasiuk.patrycja@gmail.com>
2023-09-16 22:31:59 +0200

    add seo component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import SEO from './components/Seo';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <HelmetProvider>
+        <SEO
+          title="Startupz - we create startups."
+          description="We are startup studio that develops and launches new companies."
+          name="Startupz"
+          type="article"
+          keywords="create startups, new companies, innovation, technology"/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </HelmetProvider>
     </div>
   );
 }
