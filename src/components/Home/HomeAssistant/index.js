94ff0e223f805e70646e6a7bf4b76d2fce34cc1b
qatta93 <panasiuk.patrycja@gmail.com>
2023-09-18 12:18:38 +0200

    add chat assistant section - desktop

---
+++ src/components/Home/HomeAssistant/index.js
@@ -0,0 +1,27 @@
+import React,  { useState } from "react";
+import './style.css'
+
+const HomeAssistant = () => {
+  const [closeChat, setCloseChat] = useState(false);
+
+  return (
+    <section className="home__asistant global-padding">
+      <article className="home__asistant--text">
+        <img src="/images/thumb-left.png" alt="thumb" />
+        <p>We love solving problems!</p>
+        <img src="/images/thumb-right.png" alt="thumb" />
+      </article>
+      {!closeChat &&
+        <article className="home__asistant--chat">
+          <div>
+            <img className="home__asisstant--chat-image" src="/images/chat.png" alt="chat" />
+            <img className="home__asisstant--close-image" src="/images/close.png" alt="close" onClick={() => setCloseChat(true)}/>
+          </div>
+          <img className="home__asisstant---image" src="/images/assistant.png" alt="virtual assistant" />
+        </article>
+      }
+    </section>
+  )
+}
+
+export default HomeAssistant;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HomeMain from './HomeMain/index.js';
 import HomeAbout from './HomeAbout/index.js';
+import HomeAssistant from './HomeAssistant/index.js';
 import './style.css';
 
 const Home = () => (
   <main className="home">
     <HomeMain />
     <HomeAbout />
+    <HomeAssistant />
   </main>
 );
 
