a0a51e1ae7b3b435de3656541d7376c03b8f1a09
qatta93 <panasiuk.patrycja@gmail.com>
2023-09-19 19:50:07 +0200

    accesibility improved

--- src/components/Header/CloseIcon/index.js
+++ src/components/Header/CloseIcon/index.js
@@ -9,6 +9,7 @@ const CloseIcon = () => (
     viewBox="0 0 24 24"
     stroke="currentColor"
     strokeWidth={2}
+    aria-label="close"
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
   </svg>
--- src/components/Header/HamburgerIcon/index.js
+++ src/components/Header/HamburgerIcon/index.js
@@ -9,6 +9,7 @@ const HamburgerIcon = () => (
     viewBox="0 0 24 24"
     stroke="currentColor"
     strokeWidth={2}
+    aria-label="open menu"
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
   </svg>
